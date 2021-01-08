f9ebd87e78923856f85a81e977a1994ecf8c781a
has <17601068256@163.com>
2021-01-08 22:35:51 +0800

    second

--- app/src/main/java/com/hhhmyrealmcetest/MainActivity.java
+++ app/src/main/java/com/hhhmyrealmcetest/MainActivity.java
@@ -16,9 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Realm.init(this);
-        setRealmConfiguration();
-
-"tstttt"
+        setRealmConfiguration();//adddddd
 
 //        List<PassengerTaskLine> mListline = new ArrayList<PassengerTaskLine>() ;
         RealmList<PassengerTaskLine> mListline = new RealmList<>();
